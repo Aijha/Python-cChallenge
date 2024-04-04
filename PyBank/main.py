51db14cfba3ad706d94a32be629ba51acc589f0d
Aijha <143448902+Aijha@users.noreply.github.com>
2024-04-04 16:19:02 -0700

    ...

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,23 @@
 #Main Script
+
+# The total number of months included in the dataset
+# The net total amount of "Profit/Losses" over the entire period
+
+# The changes in "Profit/Losses"
+# The average
+
+# The greatest increase in profits
+# The greatest decrease in profits 
+
+# Import Dependencies
+import os
+import csv
+
+#Specify files
+budgetcsv_path = os.path.join("../Resources/budget_data.csv")
+
+#Define Variables
+
+totalmonths = []
+netPL = []
+monthlyPL = []
